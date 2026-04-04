d40fc0b978538e85fe9bc40fe61bcead1498ad41
rangayadav939 <rangayadav939@gmail.com>
2026-04-04 14:59:09 +0530

    fix CORS

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://rangayadav939.github.io"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
